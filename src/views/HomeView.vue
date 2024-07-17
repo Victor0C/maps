7d7ebe4121c64d7b368fac8ea3a5b20009485e98
Victor Hugo <hugovco511@gmail.com>
2024-07-17 16:35:11 -0300

    feat: data that would be sent by the post

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,58 +1,64 @@
 <template>
-	<div class="container mt-5 MyContainer d-flex">
-		<form @submit.prevent="buildAddressString" class="forms me-5">
-			<div class="mb-3">
-				<label for="cep" class="form-label">CEP</label>
-				<input
-					type="text"
-					class="form-control"
-					id="cep"
-					v-model="address.cep"/>
-			</div>
-			<div class="mb-3">
-				<label for="logradouro" class="form-label">Logradouro</label>
-				<input
-					type="text"
-					class="form-control"
-					id="logradouro"
-					v-model="address.publicPlace"/>
-			</div>
-			<div class="mb-3">
-				<label for="bairro" class="form-label">Bairro</label>
-				<input
-					type="text"
-					class="form-control"
-					id="bairro"
-					v-model="address.neighborhood"/>
-			</div>
-			<div class="mb-3">
-				<label for="numero" class="form-label">N°</label>
-				<input
-					type="text"
-					class="form-control"
-					id="numero"
-					v-model="address.number"/>
-			</div>
-			<div class="mb-3">
-				<label for="complemento" class="form-label">Complemento</label>
-				<input
-					type="text"
-					class="form-control"
-					id="complemento"
-					v-model="address.complement" />
-			</div>
-			<div class="mb-3">
-				<label for="referencia" class="form-label">Ponto de Referência</label>
-				<input
-					type="text"
-					class="form-control"
-					id="referencia"
-					v-model="address.reference" />
-			</div>
-			<button type="submit" class="btn btn-primary">Enviar</button>
-		</form>
-    <Map v-if="addressString != ''" :address="addressString" @addressMap="getMapData"></Map>		
-	</div>
+  <div class="container-fluid">
+    <div class="container mt-5 d-flex">
+      <form @submit.prevent="buildAddressString" class="forms me-5">
+        <div class="mb-3">
+          <label for="cep" class="form-label">CEP</label>
+          <input
+            type="text"
+            class="form-control"
+            id="cep"
+            v-model="address.cep"/>
+        </div>
+        <div class="mb-3">
+          <label for="logradouro" class="form-label">Logradouro</label>
+          <input
+            type="text"
+            class="form-control"
+            id="logradouro"
+            v-model="address.publicPlace"/>
+        </div>
+        <div class="mb-3">
+          <label for="bairro" class="form-label">Bairro</label>
+          <input
+            type="text"
+            class="form-control"
+            id="bairro"
+            v-model="address.neighborhood"/>
+        </div>
+        <div class="mb-3">
+          <label for="numero" class="form-label">N°</label>
+          <input
+            type="text"
+            class="form-control"
+            id="numero"
+            v-model="address.number"/>
+        </div>
+        <div class="mb-3">
+          <label for="complemento" class="form-label">Complemento</label>
+          <input
+            type="text"
+            class="form-control"
+            id="complemento"
+            v-model="address.complement" />
+        </div>
+        <div class="mb-3">
+          <label for="referencia" class="form-label">Ponto de Referência</label>
+          <input
+            type="text"
+            class="form-control"
+            id="referencia"
+            v-model="address.reference" />
+        </div>
+        <button type="submit" class="btn btn-primary">Enviar</button>
+      </form>
+      <Map v-if="addressString != ''" :address="addressString" @addressMap="getMapData"></Map>		
+    </div>
+    <div class="d-flex">
+      <p class="me-2">Dados que seriam enviados no post:</p>
+      {{ this.addressWithCoordinate }}
+    </div>
+  </div>
 </template>
 
 <script>
@@ -71,7 +77,8 @@ import Map from '../components/Map.vue'
 					reference: '',
 				},
         addressString:'',
-        addressMap:{}
+        addressMap:{},
+        addressWithCoordinate:{}
 			};
 		},
     components:{
@@ -81,6 +88,9 @@ import Map from '../components/Map.vue'
       //Recupera os dados do endereço exibido no mapa
       getMapData(addressMap){
         this.addressMap = {...addressMap}
+        this.addressWithCoordinate.address = {...this.address}
+        this.addressWithCoordinate.coordinates = {...this.addressMap.coordinates}
+        
       },
 
       //Somente para simular a api de cep que é utilizada na empresa
